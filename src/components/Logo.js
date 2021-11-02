aa723dfc27e370abe63d216eb6251d9b6a32f938
WowDarkHole <isaacbartel.js@gmail.com>
2021-11-02 14:03:01 +0200

    fix: warning

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -6,8 +6,8 @@ const Logo = ({className}) => {
   return (
     <div className={parentClass}>
       <div className="relative w-40 sm:w-64 h-40 sm:h-64 p-4 rounded-full" style={{backgroundColor: '#22262A'}}>
-        <img className="animate-spin-slow w-full h-full" src="/assets/text_hero_brainchild.png" />
-        <img className="absolute w-1/4 h-1/4 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" src="/assets/logo_brain.png" />
+        <img className="animate-spin-slow w-full h-full" src="/assets/text_hero_brainchild.png" alt=""/>
+        <img className="absolute w-1/4 h-1/4 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" src="/assets/logo_brain.png" alt=""/>
       </div>
     </div>
   )
