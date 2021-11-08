07ed67c4259f8a7791228a573dd0347eb595a54d
WowDarkHole <isaacbartel.js@gmail.com>
2021-11-08 11:38:26 +0200

    fix: bug

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -5,8 +5,8 @@ const Logo = React.forwardRef(({className, slide}, ref) => {
   const parentClass = "absolute transform -translate-x-1/2 " + className + (slide > 0 && slide < 3 ? "" : " -translate-y-1/2");
 
   const parentStyle = {top: '20px'};
-  if(slide === 0) parentStyle.top = window.innerHeight - container.height;
-  if(slide === 3) parentStyle.top = container.height;
+  if(slide === 0) parentStyle.top = window.innerHeight - container?.height;
+  if(slide === 3) parentStyle.top = container?.height;
 
   const containerClass = "relative flex items-center justify-center transition-all duration-700 " + (slide > 0 && slide < 3 ? "w-20 h-20" : "w-48 sm:w-64 h-48 sm:h-64");
   const logoClass = "absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-3 rounded-full transition-all duration-700 " + (slide > 0 && slide < 3 ? "w-20 h-20" : "w-24 h-24 sm:w-28 sm:h-28");
