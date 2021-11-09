9ad27e0749d5426ea9f1c2ad84b3fc0e2d1994ea
WowDarkHole <isaacbartel.js@gmail.com>
2021-11-09 14:24:43 +0200

    add: social media link

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -21,12 +21,12 @@ const Connect = forwardRef((_, ref) => {
             </div>
             <div className={"relative flex flex-col justify-center items-center h-1/2 min-h-screen-2/5 w-full pb-20 pt-8 "+(isMobileDevice() ? "px-5" : "sm:pb-40 sm:pt-20 px-10 sm:px-20 lg:px-30")} style={{backgroundColor: '#22262A'}}>
               <embed className="w-full landscape:hidden" src="/assets/text_connect_hop_mobile.svg"/>
-              <div className="flex justify-between w-full mt-10 md:mt-0 px-8 min-w-48">
-                <embed className={"w-8 "+(isMobileDevice() ? "": "sm:w-12 xl:w-16")} src="/assets/image_connect_instagram.svg"/>
-                <embed className={"w-8 "+(isMobileDevice() ? "": "sm:w-12 xl:w-16")} src="/assets/image_connect_twitter.svg"/>
+              <div className="flex justify-between items-center w-full mt-10 md:mt-0 px-8 min-w-48">
+                <a style={{height:'max-content'}} href="https://www.instagram.com/brainchildnft/" target="_blank" rel="noreferrer"><embed className={"w-8 pointer-events-none "+(isMobileDevice() ? "": "sm:w-12 xl:w-16")} src="/assets/image_connect_instagram.svg"/></a>
+                <a style={{height:'max-content'}} href="https://twitter.com/BrainchildNFT" target="_blank" rel="noreferrer"><embed className={"w-8 pointer-events-none "+(isMobileDevice() ? "": "sm:w-12 xl:w-16")} src="/assets/image_connect_twitter.svg"/></a>
                 <embed className="w-3/5 hidden landscape:block" src="/assets/text_connect_hop.svg"/>
-                <embed className={"w-8 "+(isMobileDevice() ? "": "sm:w-12 xl:w-16")} src="/assets/image_connect_discord.svg"/>
-                <embed className={"w-8 "+(isMobileDevice() ? "": "sm:w-12 xl:w-16")} src="/assets/image_connect_telegram.svg"/>
+                <a style={{height:'max-content'}} href="https://discord.gg/7S55rjvxm3" target="_blank" rel="noreferrer"><embed className={"w-8 pointer-events-none "+(isMobileDevice() ? "": "sm:w-12 xl:w-16")} src="/assets/image_connect_discord.svg"/></a>
+                <a style={{height:'max-content'}} href="https://t.me/joinchat/KxQp5cc1K35lM2Jl" target="_blank" rel="noreferrer"><embed className={"w-8 pointer-events-none "+(isMobileDevice() ? "": "sm:w-12 xl:w-16")} src="/assets/image_connect_telegram.svg"/></a>
               </div>
             </div>
           </div>
