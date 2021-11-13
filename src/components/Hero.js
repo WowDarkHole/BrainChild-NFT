42c798bd3fbf4d0fa99b1e4d8757425f470a6a5b
WowDarkHole <isaacbartel.js@gmail.com>
2021-11-12 18:13:46 -0800

    fix: hero section bug

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,7 +2,7 @@ import React, { forwardRef } from 'react';
 
 const Hero = forwardRef((_, ref) => {
   return (
-    <div className="relative min-h-screen" ref={ref.containerRef}>
+    <div className="relative h-screen" ref={ref.containerRef}>
       <div className="relative h-full" style={{minHeight: 'max(450px, 100vh)'}}>
         <div className="absolute bottom-0 w-full pointer-events-none" ref={ref.contentRef}>
           <div className="relative flex items-center justify-center h-1/2 pb-0 pt-12 px-0 landscape:px-16 sm:pt-12 sm:pb-0 sm:px-28" style={{backgroundColor: '#22262A', minHeight: '60vh'}}>
