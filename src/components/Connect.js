46f73db72149f40b1ce0d998055fb6fd370b20ef
WowDarkHole <isaacbartel.js@gmail.com>
2021-11-16 10:09:00 -0800

    enhance: social media links

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -20,23 +20,31 @@ const Connect = forwardRef((_, ref) => {
           </div>
           <div className={"relative flex flex-col justify-center items-center h-1/2 min-h-screen-2/5 w-full pb-24 pt-8 "+(isMobileDevice() ? "px-5" : "sm:pb-28 sm:pt-20 px-10 sm:px-20 lg:px-30")} style={{backgroundColor: '#22262A'}}>
             <embed className={"w-72 sm:w-96 "+(isMobileDevice() ? "md:hidden": "md:hidden")} src="/assets/text_connect_hop_mobile.svg"/>
-            <div className={"flex justify-between items-center mt-10 md:mt-0 max-w-full min-w-60 sm:min-w-80 xl:min-w-280 lg:min-w-200 md:min-w-180 landscape:px-0"+(isMobileDevice() ? "" : "")}>
+            <embed className={"hidden md:block md:w-120 lg:w-140 xl:w-160"+(isMobileDevice() ? "": "")} src="/assets/text_connect_hop.svg"/>
+            <div className={"grid grid-rows-2 grid-cols-3 justify-items-center gap-y-9 md:grid-rows-1 md:grid-cols-6 justify-between items-center mt-10 mb-15 md:mt-20 md:mb-20 min-w-80 sm:min-w-120 md:min-w-180 landscape:px-0"+(isMobileDevice() ? "" : "")}>
+              <a style={{height:'max-content'}} className="tooltip" href="https://discord.gg/7S55rjvxm3" target="_blank" rel="noreferrer">
+                <embed className={"w-8 pointer-events-none "+(isMobileDevice() ? "": "sm:w-12")} src="/assets/image_connect_discord.svg"/>
+                <span className="tooltiptext">DISCORD</span>
+              </a>
               <a style={{height:'max-content'}} className="tooltip" href="https://www.instagram.com/brainchildnft/" target="_blank" rel="noreferrer">
-                <embed className={"w-8 pointer-events-none "+(isMobileDevice() ? "": "sm:w-10")} src="/assets/image_connect_instagram.svg"/>
+                <embed className={"w-8 pointer-events-none "+(isMobileDevice() ? "": "sm:w-12")} src="/assets/image_connect_instagram.svg"/>
                 <span className="tooltiptext">INSTAGRAM</span>
               </a>
               <a style={{height:'max-content'}} className="tooltip" href="https://twitter.com/BrainchildNFT" target="_blank" rel="noreferrer">
-                <embed className={"w-8 pointer-events-none "+(isMobileDevice() ? "": "sm:w-10")} src="/assets/image_connect_twitter.svg"/>
+                <embed className={"w-8 pointer-events-none "+(isMobileDevice() ? "": "sm:w-12")} src="/assets/image_connect_twitter.svg"/>
                 <span className="tooltiptext">TWITTER</span>
               </a>
-              <embed className={"hidden md:block md:w-120 lg:w-140 xl:w-160"+(isMobileDevice() ? "": "")} src="/assets/text_connect_hop.svg"/>
-              <a style={{height:'max-content'}} className="tooltip" href="https://discord.gg/7S55rjvxm3" target="_blank" rel="noreferrer">
-                <embed className={"w-8 pointer-events-none "+(isMobileDevice() ? "": "sm:w-10")} src="/assets/image_connect_discord.svg"/>
-                <span className="tooltiptext">DISCORD</span>
-              </a>
               <a style={{height:'max-content'}} className="tooltip" href="https://t.me/joinchat/KxQp5cc1K35lM2Jl" target="_blank" rel="noreferrer">
-                <embed className={"w-8 pointer-events-none "+(isMobileDevice() ? "": "sm:w-10")} src="/assets/image_connect_telegram.svg"/>
-                <span className="tooltiptext">TELEGRAM</span>
+                <embed className={"w-8 pointer-events-none "+(isMobileDevice() ? "": "sm:w-12")} src="/assets/image_connect_telegram_announcement.svg"/>
+                <span className="tooltiptext">TELEGRAM ANNOUNCEMENT</span>
+              </a>
+              <a style={{height:'max-content'}} className="tooltip" href="https://t.me/joinchat/kSE3qhHOpMRkY2E1" target="_blank" rel="noreferrer">
+                <embed className={"w-8 pointer-events-none "+(isMobileDevice() ? "": "sm:w-12")} src="/assets/image_connect_telegram_chat.svg"/>
+                <span className="tooltiptext">TELEGRAM CHAT</span>
+              </a>
+              <a style={{height:'max-content'}} className="tooltip" href="https://www.reddit.com/r/brainchildNFT/" target="_blank" rel="noreferrer">
+                <embed className={"w-8 pointer-events-none "+(isMobileDevice() ? "": "sm:w-12")} src="/assets/image_connect_telegram.svg"/>
+                <span className="tooltiptext">REDDIT</span>
               </a>
             </div>
           </div>
